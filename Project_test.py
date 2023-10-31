1ca0541301571ce6a4c07809ee2e4d311418f948
Maksmvs <142376726+Maksmvs@users.noreply.github.com>
2023-10-31 12:19:50 +0200

    Update Project_test.py

--- Project_test.py
+++ Project_test.py
@@ -5,102 +5,105 @@ from datetime import datetime, timedelta
 
 DATA_FILE = 'personal_assistant_data.json'
 
-if os.path.exists(DATA_FILE):
-    with open(DATA_FILE, 'r') as file:
-        data = json.load(file)
-else:
-    data = {
-        'contacts': [],
-        'notes': []
-    }
+class Contact:
+    def __init__(self, name, address, phone, email, birthday):
+        self.name = name
+        self.address = address
+        self.phone = phone
+        self.email = email
+        self.birthday = birthday
 
-def save_data():
-    
-    with open(DATA_FILE, 'w') as file:
-        json.dump(data, file)
+class Note:
+    def __init__(self, text, tags):
+        self.text = text
+        self.tags = tags
 
+class PersonalAssistant:
+    def __init__(self):
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE, 'r') as file:
+                self.data = json.load(file)
+        else:
+            self.data = {
+                'contacts': [],
+                'notes': []
+            }
 
-def add_contact(name, address, phone, email, birthday):
-    contact = {
-        'name': name,
-        'address': address,
-        'phone': phone,
-        'email': email,
-        'birthday': birthday
-    }
-    data['contacts'].append(contact)
-    save_data()
+    def save_data(self):
+        with open(DATA_FILE, 'w') as file:
+            json.dump(self.data, file)
 
+    def add_contact(self, name, address, phone, email, birthday):
+        contact = {
+            'name': name,
+            'address': address,
+            'phone': phone,
+            'email': email,
+            'birthday': birthday.strftime('%Y-%m-%d')
+        }
+        self.data['contacts'].append(contact)
+        self.save_data()
 
-def find_contacts_by_name(name):
-    results = [contact for contact in data['contacts'] if name.lower() in contact['name'].lower()]
-    return results
+    def find_contacts_by_name(self, name):
+        results = [contact for contact in self.data['contacts'] if name.lower() in contact['name'].lower()]
+        return results
 
+    def upcoming_birthdays(self, days):
+        today = datetime.now()
+        upcoming_date = today + timedelta(days=days)
+        upcoming_birthdays = [contact for contact in self.data['contacts'] if datetime.strptime(contact['birthday'], '%Y-%m-%d').date() == upcoming_date.date()]
+        return upcoming_birthdays
 
-def upcoming_birthdays(days):
-    today = datetime.now()
-    upcoming_date = today + timedelta(days=days)
-    upcoming_birthdays = [contact for contact in data['contacts'] if contact['birthday'].date() == upcoming_date.date()]
-    return upcoming_birthdays
+    @staticmethod
+    def validate_phone_number(phone):
+        pattern = r'^\+380\d{9}$'
+        if re.match(pattern, phone):
+            return True
+        else:
+            print("Неправильний формат введення номера телефону. Правильний формат: +380XXXXXXXXX")
+            return False
 
-import re
+    @staticmethod
+    def validate_email(email):
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if re.match(pattern, email):
+            return True
+        else:
+            print("Неправильний формат введення email-адреси. Правильний формат: example@email.com")
+            return False
 
-def validate_phone_number(phone):
-    
-    pattern = r'^\+380\d{9}$'
-    
-    if re.match(pattern, phone):
-        return True
-    else:
-        print("Неправильний формат введення номера телефону. Правильний формат: +380XXXXXXXXX")
-        return False
+    def edit_contact(self, contact_idx, new_data):
+        if 0 <= contact_idx < len(self.data['contacts']):
+            self.data['contacts'][contact_idx].update(new_data)
+            self.save_data()
 
-def validate_email(email):
-    
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    
-    if re.match(pattern, email):
-        return True
-    else:
-        print("Неправильний формат введення email-адреси. Правильний формат: example@email.com")
-        return False
+    def delete_contact(self, contact_idx):
+        if 0 <= contact_idx < len(self.data['contacts']):
+            del self.data['contacts'][contact_idx]
+            self.save_data()
 
+    def add_note(self, text, tags):
+        note = {'text': text, 'tags': tags}
+        self.data['notes'].append(note)
+        self.save_data()
 
-def edit_contact(contact_idx, new_data):
-    if 0 <= contact_idx < len(data['contacts']):
-        data['contacts'][contact_idx].update(new_data)
-        save_data()
+    def find_notes_by_tag(self, tag):
+        results = [note for note in self.data['notes'] if tag in note['tags']]
+        return results
 
+    def edit_note(self, note_idx, new_data):
+        if 0 <= note_idx < len(self.data['notes']):
+            self.data['notes'][note_idx].update(new_data)
+            self.save_data()
 
-def delete_contact(contact_idx):
-    if 0 <= contact_idx < len(data['contacts']):
-        del data['contacts'][contact_idx]
-        save_data()
-
-
-def add_note(text):
-    note = {'text': text, 'tags': []}
-    data['notes'].append(note)
-    save_data()
-
-
-def find_notes_by_tag(tag):
-    results = [note for note in data['notes'] if tag in note['tags']]
-    return results
-
-
-def edit_note(note_idx, new_data):
-    if 0 <= note_idx < len(data['notes']):
-        data['notes'][note_idx].update(new_data)
-        save_data()
-
-
-def delete_note(note_idx):
-    if 0 <= note_idx < len(data['notes']):
-        del data['notes'][note_idx]
-        save_data()
+    def delete_note(self, note_idx):
+        if 0 <= note_idx < len(self.data['notes']):
+            del self.data['notes'][note_idx]
+            self.save_data()
 
 if __name__ == '__main__':
+    personal_assistant = PersonalAssistant()
+
     while True:
         print("1. Додати контакт")
         print("2. Пошук контактів за іменем")
@@ -117,15 +120,15 @@ if __name__ == '__main__':
             phone = input("Номер телефону: ")
             email = input("Email: ")
             birthday = input("Дата народження (рррр-мм-дд): ")
-            if validate_phone_number(phone) and validate_email(email):
-                add_contact(name, address, phone, email, datetime.strptime(birthday, '%Y-%m-%d'))
+            if personal_assistant.validate_phone_number(phone) and personal_assistant.validate_email(email):
+                personal_assistant.add_contact(name, address, phone, email, datetime.strptime(birthday, '%Y-%m-%d'))
                 print("Контакт додано.")
             else:
                 print("Некоректний номер телефону або email.")
         
         elif choice == '2':
             name = input("Пошук за іменем: ")
-            results = find_contacts_by_name(name)
+            results = personal_assistant.find_contacts_by_name(name)
             if results:
                 for idx, contact in enumerate(results):
                     print(f"{idx+1}. {contact['name']} - {contact['phone']} - {contact['email']}")
@@ -134,7 +137,7 @@ if __name__ == '__main__':
         
         elif choice == '3':
             days = int(input("Введіть кількість днів: "))
-            upcoming = upcoming_birthdays(days)
+            upcoming = personal_assistant.upcoming_birthdays(days)
             if upcoming:
                 for contact in upcoming:
                     print(f"{contact['name']} - {contact['birthday'].strftime('%Y-%m-%d')}")
@@ -143,12 +146,13 @@ if __name__ == '__main__':
         
         elif choice == '4':
             note = input("Текст нотатки: ")
-            add_note(note)
+            tags = input("Теги (розділені комами): ").split(',')
+            personal_assistant.add_note(note, tags)
             print("Нотатку додано.")
         
         elif choice == '5':
             tag = input("Пошук за тегом: ")
-            results = find_notes_by_tag(tag)
+            results = personal_assistant.find_notes_by_tag(tag)
             if results:
                 for idx, note in enumerate(results):
                     print(f"{idx+1}. {note['text']}")
